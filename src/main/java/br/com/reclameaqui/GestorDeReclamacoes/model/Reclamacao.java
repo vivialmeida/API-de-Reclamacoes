f3ef7d7a41dc49d25e5a9a10c5e26b8fdd9f13e9
vivialmeida <viviane_k5@hotmail.com>
2021-05-14 10:03:51 -0300

    Adicionando configuracoes do banco de dados

--- src/main/java/br/com/reclameaqui/GestorDeReclamacoes/model/Reclamacao.java
+++ src/main/java/br/com/reclameaqui/GestorDeReclamacoes/model/Reclamacao.java
@@ -1,14 +1,21 @@
 package br.com.reclameaqui.GestorDeReclamacoes.model;
 
 import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
 @Data
+@Document(collection = "reclamacao")
 public class Reclamacao {
 
+  @Id
+  private Long id;
   private String titulo;
   private String descricao;
   private Localidade localidade;
   private Empresa empresa;
   private LocalDateTime datahora;
+
 }
